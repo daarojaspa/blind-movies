b95dbee67577cec71e1c1ef3fb087f2db072aa68
Dan <daarojaspa@unal.edu.co>
2024-11-04 11:09:38 -0500

    add categories

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-require('dotenv') .config()
+//import { API_KEYS } from "./secret";
+//console.log(process.env.API_KEY)
 async function  getTrandingHome(){
-const Url= 'https://api.themoviedb.org/3/trending/movie/week?api_key=';
     const request=await fetch(Url);
     const data= await request.json();
     const movies = data.results;
@@ -17,4 +17,28 @@ const Url= 'https://api.themoviedb.org/3/trending/movie/week?api_key=';
             moviesPrevewTrandingSection.appendChild(movieContainer);
         });
 }
+
+async function getCategories(){
+    const request=await fetch(Url);
+    const data= await request.json();
+    const categories=data.genres
+    categories.forEach(
+        (category) =>{
+
+                const previewCategoriesContainer= document.querySelector('#categoriesPreview .categoriesPreview-list')
+                const categoryContainer=document.createElement('div');
+                categoryContainer.classList.add('category-container');
+                const header=document.createElement('h3');
+                header.classList.add('category-title')
+                header.setAttribute('id','id'+category.id)
+                const headerText=document.createTextNode(category.name);
+                header.appendChild(headerText);
+                categoryContainer.appendChild(header);
+                previewCategoriesContainer.appendChild(categoryContainer);
+                
+               }
+    );
+
+}
 getTrandingHome();
+getCategories();
